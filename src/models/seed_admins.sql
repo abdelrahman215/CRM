cc9625184749bb1ad1e76dd2acad5ae29496c354
abdelrahman215 <boodymokhtar03@gmail.com>
2026-03-15 20:46:41 +0200

    Delete src directory

--- src/models/seed_admins.sql
+++
@@ -1,9 +0,0 @@
-INSERT INTO users (name, email, password, role)
-VALUES
-  ('Admin One', 'Admin1@admin.com', '$2b$10$0louRyxCfC20ssciElogEOf58t1SpK9UVw6tGlvfVSSuSdeVe..re', 'admin'),
-  ('Admin Two', 'Admin2@admin.com', '$2b$10$0louRyxCfC20ssciElogEOf58t1SpK9UVw6tGlvfVSSuSdeVe..re', 'admin'),
-  ('Admin Three', 'Admin3@admin.com', '$2b$10$0louRyxCfC20ssciElogEOf58t1SpK9UVw6tGlvfVSSuSdeVe..re', 'admin')
-ON CONFLICT (email) DO NOTHING;
-
--- Password hash corresponds to: Admin@123
-
--- src/pages/SalesPerformance.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import SalesDashboard from './SalesDashboard';
-
-export default function SalesPerformance() {
-  return <SalesDashboard />;
-}
-
